8be57def01cc53ff766291a5e9a4c1f565bc60d1
Kalev Härk <kalevhark@gmail.com>
2019-06-04 21:17:36 +0300

    static ja media failide asukoha seaded

--- kroonika/settings.py
+++ kroonika/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static/'
+STATIC_URL = '/kroonika/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/kroonika/media/'
 
 def FILTERS_VERBOSE_LOOKUPS():
     from django_filters.conf import DEFAULTS
