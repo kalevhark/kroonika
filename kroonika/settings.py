a9fe5910cd5565f9ed68f0006b549829cbac5a97
Kalev Härk <kalevhark@gmail.com>
2019-06-19 11:33:58 +0300

    reCAPTCHA

--- kroonika/settings.py
+++ kroonika/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = '05655)*fc)5t$^u6-b(8*8#1njf=)4y@eesgsz-o@spx6rzcmo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.196.203.237', '127.0.0.1', '192.168.1.143']
+ALLOWED_HOSTS = [
+    '18.196.203.237',
+    '127.0.0.1',
+    '192.168.1.143',
+    'deb9server.lan'
+]
 
 
 # Application definition
