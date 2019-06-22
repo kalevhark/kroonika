0d290274250df2bc6473d9bdb5edc810ae8f6346
Kalev Härk <kalevhark@gmail.com>
2019-06-23 01:07:04 +0300

    Objekti muutmise vormi täiendus

--- wiki/forms.py
+++ wiki/forms.py
@@ -83,7 +83,6 @@ class ObjektForm(ModelForm):
         # )
         widgets = {
             'kirjeldus': Textarea(attrs={'cols': 40, 'rows': 5}),
-            'objektid': FilteredSelectMultiple(attrs={'size': 15, 'title': 'Vali seotud objektid'}),
-            # 'objektid': SelectMultiple(attrs={'size': 15, 'title': 'Vali seotud objektid'}),
+            'objektid': SelectMultiple(attrs={'size': 15, 'title': 'Vali seotud objektid'}),
             'viited': SelectMultiple(attrs={'size': 15, 'title': 'Vali seotud viited'}),
         }
