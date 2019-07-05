25e04f51d21158f3e502d7dbfe8201d63eda9dce
Kalev Härk <kalevhark@gmail.com>
2019-07-05 15:32:38 +0300

    Vigase sisuga artikli juurde märge Objekt, Organisatsioo + refactoring

--- kroonika/urls.py
+++ kroonika/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 # from django.contrib.auth import views as auth_views
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import routers, serializers, viewsets
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+# from rest_framework import routers, serializers, viewsets
 from rest_framework.schemas import get_schema_view
 
 from .routers import router
@@ -35,6 +36,7 @@ urlpatterns = [
 	path('api/', include(router.urls)),
     path('schema/', schema_view),
     path('wiki/', include('wiki.urls')),
+    path(r'^robots\.txt$', TemplateView.as_view(template_name="kroonika/robots.txt", content_type='text/plain'))
     ]
 
 # Serve static files
