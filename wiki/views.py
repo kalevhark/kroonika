72d24e16fc495f534726ac7d63848f7670325064
Kalev Härk <kalevhark@gmail.com>
2019-07-31 22:56:10 +0300

    Objekt, Organisatsioon sünniaja näitamine

--- wiki/views.py
+++ wiki/views.py
@@ -333,7 +333,7 @@ def algus(request):
             nulliga=ExpressionWrapper(
                 (datetime.date.today().year - F('hist_year'))%5, output_field=IntegerField()),
             vanus_gen=ExpressionWrapper(
-                    datetime.date.today().year - (ExtractYear('hist_date') if 'hist_date' else F('hist_year')), output_field=IntegerField())).filter(nulliga=0).order_by('-vanus_gen')
+                    datetime.date.today().year - (ExtractYear('hist_date') if 'hist_date' else 'hist_year'), output_field=IntegerField())).filter(nulliga=0).order_by('-vanus_gen')
         a['juubilarid'] = juubilarid
     andmed['objekt'] = a
     
