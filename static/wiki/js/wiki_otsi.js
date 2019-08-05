248328956c69a4a1604dd860e8f836a62ec05e86
Kalev Härk <kalevhark@gmail.com>
2019-08-06 00:46:09 +0300

    Otsingulehe lisamine

--- static/wiki/js/wiki_otsi.js
+++ static/wiki/js/wiki_otsi.js
@@ -38,7 +38,8 @@ var watchExampleVM = new Vue({
           return
         };
         this.answer = 'Otsime...';
-        var kroonika_url = window.kroonika_url; //  'http://192.168.1.143/kroonika/';
+        console.log(window.kroonika_url);
+        var kroonika_url = 'http://' + window.kroonika_url; //  'http://192.168.1.143/kroonika/';
         var isik_api_url = kroonika_url + 'api/isik/?format=json';
         var organisatsioon_api_url = kroonika_url + 'api/organisatsioon/?format=json';
         var objekt_api_url = kroonika_url + 'api/objekt/?format=json';
