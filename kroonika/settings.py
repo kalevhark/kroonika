85eb80b4c8686b987cf62043313af7d161b45440
Kalev Härk <kalevhark@gmail.com>
2019-10-19 01:38:09 +0300

    Parandatud seaded aws jaoks

--- kroonika/settings.py
+++ kroonika/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '18.196.203.237',
-    '18.190.26.213',
+    '18.190.26.217',
     '127.0.0.1', 'localhost',
     '192.168.1.143', 'deb9server.lan'
 ]
