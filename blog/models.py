5582d61ba9c2bc2e08113cf6348e0a4fd7857a2e
Kalev Härk <kalevhark@gmail.com>
2019-12-25 16:12:51 +0200

    Markdown lisamine

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
@@ -12,13 +14,25 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
+    # body = models.TextField()
+    body = MarkdownxField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
 
     def __str__(self):
         return self.title
+
+    # Create a property that returns the markdown instead
+    @property
+    def formatted_markdown(self):
+        return markdownify(self.body)
+
+    def body_summary(self):
+        summary = self.body
+        if summary.find('\n') > 0:
+            summary = summary[:summary.find('\n')]
+        return markdownify(summary[:300] + "...")
 
     class Meta:
         verbose_name_plural = "Postitused"
--- kroonika/settings.py
+++ kroonika/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'captcha',
     'crispy_forms',
+    'markdownx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
