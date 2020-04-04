d53a0b5a64bd348c7739f66768cbf58c86df52cd
kalevhark <Kalev.Hark@mail.ee>
2020-04-04 21:34:09 +0300

    massikanne

--- tools.py
+++ tools.py
@@ -105,7 +105,7 @@ def join(model_name, source_id, dest_id):
 # tools.massikanne()
 def massikanne():
     # Millised isikud lisada artiklile
-    isik_str = 'Jaan Ilwes, Karl Walk, Juhan Warik, August Niilus'
+    isik_str = 'Jaan Susi, Aadu Pukk, Simon Königsberg, Jaan Kütt, Paul Pulst, Jaan Must, Mehka Parts, Karl Milk'
     # Millise artikliga siduda isik
     art = Artikkel.objects.get(id=793)
     print(art)
@@ -119,7 +119,7 @@ def massikanne():
     viide = Viide.objects.get(id=8082)
     print(viide)
     # Isiku kirjeldus
-    isik_kirjeldus = 'Riikliku ehituslaenu saajad'
+    isik_kirjeldus = 'Põlisele rendile maasaajad'
     isikud = isik_str.split(',')
     for isik in isikud:
         # Loome uue isiku
