9ce1426e86c4c03975e278b189f7614cdbe4f41c
kalevhark <Kalev.Hark@mail.ee>
2020-04-28 13:55:19 +0300

    urllib error handling

--- ilm/utils/update_db.py
+++ ilm/utils/update_db.py
@@ -11,8 +11,8 @@ import re
 import sys
 import xml.etree.ElementTree as ET
 
-# from urllib.request import Request, urlopen
-import urllib
+from urllib.request import Request, urlopen
+from urllib.error import URLError
 
 from bs4 import BeautifulSoup
 import psycopg2
@@ -87,47 +87,52 @@ def ilmaandmed_veebist(dt):
          '&filter[hour]=',
          tund]
     )
-    # Loeme veebist andmed
-    try:
-        req = urllib.request.Request(p2ring, headers={'User-Agent': 'Mozilla/5.0'})
-        webpage = urllib.request.urlopen(req).read()
-    except urllib.error.URLError as e:
-        print(e.reason)
-        return None
-    # Struktueerime
-    soup = BeautifulSoup(webpage, 'html.parser')
-    kontroll_hour = soup.find(attrs={"name": "filter[hour]"})
-    kontroll_date = soup.find(attrs={"name": "filter[date]"})
     andmed = dict()
-    if kontroll_hour:
-        if kontroll_hour['value'].zfill(2) != tund.zfill(2) or kontroll_date['value'] != p2ev:
-            print(dt, 'Vale!')
-            # Kui vastus vale kellaajaga või kuupäevaga, saadame tagasi tühja tabeli
-            return andmed
-    # Leiame lehelt tabeli
-    table = soup.table
-    # Leiame tabelist rea
-    row = table.find(string=re.compile(jaam))
-    data = row.find_parent().find_next_siblings()
-    for i in range(len(data)):
-        if data[i]: # kui andmeväli pole tühi
-            if cols[i] in ['phenomenon', 'phenomenon_observer']: # tekstiväli
-                andmed[cols[i]] = data[i].text.strip()
-            else: # numbriväli
-                value = data[i].text.strip().replace(',', '.')
-                andmed[cols[i]] = float_or_none(value)
-        else:
-            andmed[cols[i]] = None
+    # Loeme veebist andmed
+    req = Request(p2ring, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        response = urlopen(req).read()
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+    else:
+        # Struktueerime
+        soup = BeautifulSoup(response, 'html.parser')
+        kontroll_hour = soup.find(attrs={"name": "filter[hour]"})
+        kontroll_date = soup.find(attrs={"name": "filter[date]"})
+        if kontroll_hour:
+            if kontroll_hour['value'].zfill(2) != tund.zfill(2) or kontroll_date['value'] != p2ev:
+                print(dt, 'Vale!')
+                # Kui vastus vale kellaajaga või kuupäevaga, saadame tagasi tühja tabeli
+                return andmed
+        # Leiame lehelt tabeli
+        table = soup.table
+        # Leiame tabelist rea
+        row = table.find(string=re.compile(jaam))
+        data = row.find_parent().find_next_siblings()
+        for i in range(len(data)):
+            if data[i]: # kui andmeväli pole tühi
+                if cols[i] in ['phenomenon', 'phenomenon_observer']: # tekstiväli
+                    andmed[cols[i]] = data[i].text.strip()
+                else: # numbriväli
+                    value = data[i].text.strip().replace(',', '.')
+                    andmed[cols[i]] = float_or_none(value)
+            else:
+                andmed[cols[i]] = None
 
-    # andmed['station'] = Jaam.objects.filter(name=jaam).first()
-    andmed['station_id'] = 1
-    andmed['timestamp'] = pytz.timezone('Europe/Tallinn').localize(
-        datetime(dt.year, dt.month, dt.day, dt.hour))
-    # Ilmaandmed andmebaasi juhul kui põhiandmed olemas
-    # if andmed['airtemperature'] != None:
-    #     i = Ilm(**andmed)
-    #     i.save()
-    #     print('Salvestan andmebaasi:', d)
+        # andmed['station'] = Jaam.objects.filter(name=jaam).first()
+        andmed['station_id'] = 1
+        andmed['timestamp'] = pytz.timezone('Europe/Tallinn').localize(
+            datetime(dt.year, dt.month, dt.day, dt.hour))
+        # Ilmaandmed andmebaasi juhul kui põhiandmed olemas
+        # if andmed['airtemperature'] != None:
+        #     i = Ilm(**andmed)
+        #     i.save()
+        #     print('Salvestan andmebaasi:', d)
     return andmed
 
 # The following connect() function connects to the suppliers database and prints out the PostgreSQL database version.
