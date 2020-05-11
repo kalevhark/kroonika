eee35acba0baf1fc5ba035a7f9b5a0ab357033ea
kalevhark <Kalev.Hark@mail.ee>
2020-05-11 18:33:43 +0300

    Ajutine Organisatsioon perf problem

--- wiki/models.py
+++ wiki/models.py
@@ -121,15 +121,12 @@ class DaatumitegaManager(models.Manager):
         else:
             # Kui andmebaas on Isik, Organisatsioon, Objekt
             if not (request.user.is_authenticated and request.user.is_staff):
-                if initial_queryset.model.__name__ == 'Organisatsioons':
-                    filtered_queryset = initial_queryset  # TODO: Ajutine lahendus
-                else:
-                    artikkel_qs = Artikkel.objects.daatumitega(request)
-                    filtered_queryset = initial_queryset.filter(
-                        Q(viited__isnull=False) |
-                        Q(viited__isnull=True, artikkel__isnull=True) |
-                        Q(artikkel__in=artikkel_qs)
-                    ).distinct()
+                artikkel_qs = Artikkel.objects.daatumitega(request)
+                filtered_queryset = initial_queryset.filter(
+                    Q(viited__isnull=False) |
+                    Q(viited__isnull=True, artikkel__isnull=True) |
+                    Q(artikkel__in=artikkel_qs)
+                ).distinct()
             else:
                 filtered_queryset = initial_queryset
 
