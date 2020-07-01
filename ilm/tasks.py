c2f5b725b3fd85d6698b39dd32cb2e57835bcede
kalevhark <Kalev.Hark@mail.ee>
2020-07-01 14:39:43 +0300

    ilm forecast

--- ilm/tasks.py
+++ ilm/tasks.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
         i_dt = None
         i_temp = None
         i_prec = None
-        i_data = i.get(str(ref_dt), None)
+        i_data = i['forecast'].get(str(ref_dt), None)
         if i_data:
             i_dt = i_data['timestamp']
             i_temp = i_data['temperature']
