bd62e445376bf766f68d3fa37d11b0bf1bde100e
kalevhark <Kalev.Hark@mail.ee>
2020-12-11 07:25:27 +0200

    test

---
+++ proov.py
@@ -0,0 +1,15 @@
+from django.test.utils import setup_test_environment
+setup_test_environment()
+
+from django.test import Client
+
+# create an instance of the client for our use
+client = Client()
+# get a response from '/'
+response = client.get('/')
+print(response.status_code)
+
+from django.urls import reverse
+response = client.get(reverse('algus'))
+print(response.status_code)
+print(response.context.keys())
--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,13 @@
+from django.urls import reverse
 from django.test import TestCase
+# from unittest import TestCase
 
 # Create your tests here.
+# from .models import Question
+
+class AlgusViewTests(TestCase):
+   def test_index_view(self):
+       response = self.client.get(reverse('special_j6ul2020'))
+       self.assertEqual(response.status_code, 200)
+       # self.assertContains(response, "No polls are available.")
+       # self.assertQuerysetEqual(response.context['latest_question_list'], [])
