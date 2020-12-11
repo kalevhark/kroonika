7b9fba8793e953903bad811c506334a265961214
kalevhark <Kalev.Hark@mail.ee>
2020-12-11 14:28:35 +0200

    test

--- proov.py
+++ proov.py
@@ -1,24 +1,24 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'kroonika.settings'
 
-# import django
-# django.setup()
-
-# from django.conf import settings
-# settings.configure()
-
-from django.test.utils import setup_test_environment
-setup_test_environment()
-
+import django
 from django.test import Client
+from django.test.utils import setup_test_environment
+from django.urls import reverse
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'kroonika.settings'
+django.setup()
+setup_test_environment()
 
 # create an instance of the client for our use
 client = Client()
+
 # get a response from '/'
 response = client.get('/')
 print(response.status_code)
 
-from django.urls import reverse
-response = client.get(reverse('algus'))
-print(response.status_code)
-print(response.context.keys())
+pages = ['algus', 'ilm', 'info']
+
+for page in pages:
+    response = client.get(reverse(page))
+    print(page, response.status_code)
+    print(response.context.keys())
