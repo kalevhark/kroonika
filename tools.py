b78ff2412882370fb69b663a0b098d2bb6fe85a0
kalevhark <Kalev.Hark@mail.ee>
2021-05-15 08:54:06 +0300

    massikanne

--- tools.py
+++ tools.py
@@ -246,23 +246,23 @@ def massikanne_from_xml():
 def massikanne_from_data():
     # Millised isikud lisada artiklile
     isik_str = """
-Melanie Aben, Johanna Adele Asserus, Emilie Elias, Molly Elias, Elisabeth Falkenberg, Helmi Ilisson, Aino Kangro, Elviira Leonora Kelt, Ludmilla Kunts, Karoline Kõiv, Nadežda Kütt, Klaudia Leetus, Alvine Liiv, Ludmilla Luik, Elfriede Marie Noor, Ida Oja, Elsa Pabstel, Hilda Emilie Pavel, Linda Marie Peet, Linda Päss, Lonny Rebane, Irina Ruga, Marie Simson, Ella Tänna, Selma Väin
+Marta Wilzin, Waleria Sonne, Jaan Bakkis, Rudolf Laatsis, Adolf Briedis, August Melder, Artur Murniek
     """
     # Millise artikliga siduda isik
     art = Artikkel.objects.get(id=9589)
     print(art)
     # Millise pildiga siduda isik
-    pilt = Pilt.objects.get(id=6070)
+    pilt = Pilt.objects.get(id=6071)
     print(pilt)
     # Milline organisatsioon lisada isikule
-    org = Organisatsioon.objects.get(id=33) # 33=tüt gümn, 85=poeg gymn, 2736=vene gymn, 2770=läti kesk, saksa eragymn, 19=6.algkool
+    org = Organisatsioon.objects.get(id=2770) # 33=tüt gümn, 85=poeg gymn, 2736=vene gymn, 2770=läti kesk, saksa eragymn, 19=6.algkool
     print(org)
     # Milline viide lisada isikule
-    viited_ids = [10560, 10561]
+    viited_ids = [10560]
     viited = Viide.objects.filter(id__in=viited_ids)
     print(viited)
     # Isiku kirjeldus
-    isik_kirjeldus = 'Tütarlaste gümnaasiumi majapidamisharu lõpetaja 1929'
+    isik_kirjeldus = 'Valga Läti Ühishumanitaargümnaasiumi lõpetaja 1929'
     isikud = isik_str.split(',')
     for isik in isikud:
         # Loome uue isiku
