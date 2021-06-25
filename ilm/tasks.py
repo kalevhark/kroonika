3fdd7425299f46ada7636a34f0e0e5aaa9bfc03a
kalevhark <Kalev.Hark@mail.ee>
2021-06-25 19:39:27 +0300

    ilm uncomplete update

--- ilm/tasks.py
+++ ilm/tasks.py
@@ -225,7 +225,7 @@ def update_uncomplete_observations(path=''):
         # create a new cursor
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         # leiame jooksva aasta ilma õhutemperatuuri näiduta read
-        cur.execute("SELECT id, timestamp FROM ilm_ilm WHERE date_part('year', timestamp) = date_part('year', CURRENT_DATE) AND airtemperature IS NULL;")
+        # cur.execute("SELECT id, timestamp FROM ilm_ilm WHERE date_part('year', timestamp) = date_part('year', CURRENT_DATE) AND airtemperature IS NULL;")
         # leiame kõik ilma õhutemperatuuri näiduta read
         cur.execute(
             "SELECT id, timestamp FROM ilm_ilm WHERE airtemperature IS NULL;"
