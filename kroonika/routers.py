965d55f2b0b30029eb4b88f9314a28a2e9d79703
kalevhark <Kalev.Hark@mail.ee>
2022-02-12 20:45:39 +0200

    API facelift

--- kroonika/routers.py
+++ kroonika/routers.py
@@ -17,38 +17,7 @@ from wiki.viewsets import (
 from ilm.viewsets import (
     IlmViewSet,
     JaamViewSet,
-    # ForecastsViewSet
 )
-
-# from rest_framework.routers import Route, DynamicRoute, SimpleRouter
-
-# class CustomReadOnlyRouter(SimpleRouter):
-#     """
-#     A router for read-only APIs, which doesn't use trailing slashes.
-#     """
-#     routes = [
-#         Route(
-#             url=r'^{prefix}$',
-#             mapping={'get': 'list'},
-#             name='{basename}-list',
-#             detail=False,
-#             initkwargs={'suffix': 'List'}
-#         ),
-#         Route(
-#             url=r'^{prefix}/{lookup}$',
-#             mapping={'get': 'retrieve'},
-#             name='{basename}-detail',
-#             detail=True,
-#             initkwargs={'suffix': 'Detail'}
-#         ),
-#         DynamicRoute(
-#             url=r'^{prefix}/{lookup}/{url_path}$',
-#             name='{basename}-{url_name}',
-#             detail=True,
-#             initkwargs={}
-#         )
-#     ]
-
 
 class MyAPIRootView(routers.APIRootView):
     """
@@ -69,8 +38,6 @@ class MyAPIRootView(routers.APIRootView):
 class MyRouter(routers.DefaultRouter):
     APIRootView = MyAPIRootView
 
-# router = routers.DefaultRouter()
-# router = CustomReadOnlyRouter()
 router = MyRouter()
 
 # wiki
@@ -87,7 +54,3 @@ router.register(r'viide', ViideViewSet)
 # ilm
 router.register(r'i', IlmViewSet)
 router.register(r'j', JaamViewSet)
-# router.register(r'forecasts', ForecastsViewSet)
-
-# router.get_api_root_view().cls.__name__ = "valgalinn.ee API"
-# router.get_api_root_view().cls.__doc__ = "API päringute tegemiseks Valga linna kroonika andmebaasist"
