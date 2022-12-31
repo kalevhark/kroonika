0441250f17f222d6560e352c418583b3b947011f
kalevhark <Kalev.Hark@mail.ee>
2022-12-31 13:45:02 +0200

    map control redesign

--- wiki/utils/wiki_kaart.js
+++ wiki/utils/wiki_kaart.js
@@ -17,4 +17,48 @@ function showKaartAasta(map) {
   }
 }
 
+function relocateElementBySelector(elementSelector, destSelector) {
+  let element = elementSelector.childNodes[0];
+  element.getElementsByClassName('kaart-control-layers')[0].innerText = 'tuvastatud kohad';
+  let elementParent = element.parentElement;
+  let destElement = destSelector;
+  elementParent.removeChild(element);
+  const node = document.createTextNode(" ");
+  destElement.appendChild(node);
+  destElement.appendChild(element);
+}
+
+// t6stame kaardikihtide k6rvale kaardiobjektide n2itamise lyliti
+function redesignControl() {
+  const leafletControlLayersListDiv = document.getElementsByClassName('leaflet-control-layers-list')[0];
+  // console.log(leafletControlLayersListDiv);
+
+  const leafletControlLayersBaseDiv = leafletControlLayersListDiv.getElementsByClassName('leaflet-control-layers-base')[0];
+  // console.log(leafletControlLayersBaseDiv);
+
+  const leafletControlLayersOverlaysDiv = leafletControlLayersListDiv.getElementsByClassName('leaflet-control-layers-overlays')[0];
+  // console.log(leafletControlLayersOverlaysDiv);
+
+  const leafletControlLayersSeparatorDiv = leafletControlLayersListDiv.getElementsByClassName('leaflet-control-layers-separator')[0];
+  // console.log(leafletControlLayersSeparatorDiv);
+
+  const allLeafletControlLayersBaseLabels = leafletControlLayersBaseDiv.getElementsByTagName('label');
+  for (let i = 0; i < allLeafletControlLayersBaseLabels.length; i++) {
+    // console.log(allLeafletControlLayersBaseLabels[i].textContent);
+    var allLeafletControlLayersOverlaysLabels = leafletControlLayersOverlaysDiv.getElementsByTagName('label');
+    for (let j = 0; j < allLeafletControlLayersOverlaysLabels.length; j++) {
+      // console.log(allLeafletControlLayersOverlaysLabels[j].textContent);
+      if ( allLeafletControlLayersOverlaysLabels[j].textContent.includes(allLeafletControlLayersBaseLabels[i].textContent) ) {
+        // console.log(allLeafletControlLayersOverlaysLabels[j].textContent);
+        relocateElementBySelector(allLeafletControlLayersOverlaysLabels[j], allLeafletControlLayersBaseLabels[i]);
+      }
+    }
+  }
+}
+
 showKaartAasta(map);
+redesignControl()
+
+// map.on('baselayerchange', function (eventLayer) {
+  // console.log(eventLayer);
+// });
