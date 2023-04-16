75ff88d336b1905f6b480d749d1b4c4905938617
kalevhark <Kalev.Hark@mail.ee>
2023-04-16 10:01:23 +0300

    add new user email to admin

--- wiki/apps.py
+++ wiki/apps.py
@@ -2,3 +2,4 @@ from django.apps import AppConfig
 
 class WikiConfig(AppConfig):
     name = 'wiki'
+
--- wiki/models.py
+++ wiki/models.py
@@ -2216,3 +2216,36 @@ class Kaardiobjekt(models.Model):
     class Meta:
         ordering = ['kaart', 'tn', 'nr']
         verbose_name_plural = "Kaardiobjektid"
+
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+#signal used for is_active=False to is_active=True
+# @receiver(pre_save, sender=User, dispatch_uid='active')
+# def active(sender, instance, **kwargs):
+#     if instance.is_active and User.objects.filter(pk=instance.pk, is_active=False).exists():
+#         subject = 'Active account'
+#         message = '%s your account is now active' %(instance.username)
+#         from_email = settings.EMAIL_HOST_USER
+#         print('2active', message, from_email)
+#         # send_mail(subject, message, from_email, [instance.email], fail_silently=False)
+
+#signal to send an email to the admin when a user creates a new account
+@receiver(post_save, sender=User, dispatch_uid='register')
+def register(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        subject = 'Uus kasutaja: %s' %(instance.username)
+        message = 'Uus kasutaja: %s' %(instance.username)
+        from_email, to = f'valgalinn.ee <{settings.DEFAULT_FROM_EMAIL}>', 'kalevhark@gmail.com'
+        merge_data = {
+            'message': message
+        }
+        html_content = render_to_string('wiki/email/feedback2admin.html', merge_data)
+        msg = EmailMultiAlternatives(subject, message, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send(fail_silently=False)
