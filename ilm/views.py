8abc73db588ea67c520b6f3497cd1bbaa5b60a31
kalevhark <Kalev.Hark@mail.ee>
2024-02-24 21:05:15 +0200

    analyze + prec zero

--- ilm/views.py
+++ ilm/views.py
@@ -135,6 +135,12 @@ def float_or_none(value):
     except:
         return None
 
+def float_or_zero(value):
+    try:
+        return float(value)
+    except:
+        return 0
+
 def index(request):
     # Avalehekülg, kus näidatakse 24h ilmaajalugu + 48h prognoos
     weather = utils.owm_onecall()
@@ -2248,7 +2254,7 @@ def get_forecasts():
         if y_data:
             y_temp = y_data['temperature']
             y_icon = y_data['symbol']
-            y_prec = float_or_none(y_data['precipitation'])
+            y_prec = float_or_zero(y_data['precipitation'])
             y_prec_color = y_data['precipitation_color']
             y_pres = float_or_none(y_data['pressure'])
         # openweathermaps.org
@@ -2262,7 +2268,7 @@ def get_forecasts():
             o_temp = o_data['temp']
             o_icon = o_data['weather'][0]['icon']
             try:
-                o_prec = float_or_none(o_data['rain']['1h'])
+                o_prec = float_or_zero(o_data['rain']['1h'])
             except:
                 o_prec = None
             o_prec_color = o_data['precipitation_color']
@@ -2277,7 +2283,7 @@ def get_forecasts():
         i_data = i['forecast'].get(str(ref_dt), None)
         if i_data:
             i_temp = float_or_none(i_data['temperature'])
-            i_prec = float_or_none(i_data['precipitation'])
+            i_prec = float_or_zero(i_data['precipitation'])
             i_prec_color = i_data['precipitation_color']
             i_pres = float_or_none(i_data['pressure'])
             i_icon = i_data['symbol']
--- logs/analyze.py
+++ logs/analyze.py
@@ -53,6 +53,67 @@ def logfile2df(logfile):
     # Tagastame ainult vajalikud veerud
     return df.drop(['time_str'], axis=1)
 
+
+def parse_str(x):
+    """
+    Returns the string delimited by two characters.
+    Example:
+        `>>> parse_str('[my string]')`
+        `'my string'`
+    """
+    if x is None:
+        print("X : ", x)
+        return "AAAAAAAAAAAAAAA"
+    return x[1:-1]
+
+
+def parse_datetime(x):
+    '''
+    Parses datetime with timezone formatted as:
+        `[day/month/year:hour:minute:second zone]`
+    Example:
+        `>>> parse_datetime('13/Nov/2015:11:45:42 +0000')`
+        `datetime.datetime(2015, 11, 3, 11, 45, 4, tzinfo=<UTC>)`
+    Due to problems parsing the timezone (`%z`) with `datetime.strptime`, the
+    timezone will be obtained using the `pytz` library.
+    '''
+    try:
+        dt = datetime.strptime(x[1:-7], '%d/%b/%Y:%H:%M:%S')
+        dt_tz = int(x[-6:-3]) * 60 + int(x[-3:-1])
+        return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
+    except:
+        x = "[02/May/2021:03:20:40 +0700]"
+        dt = datetime.strptime(x[1:-7], '%d/%b/%Y:%H:%M:%S')
+        dt_tz = int(x[-6:-3]) * 60 + int(x[-3:-1])
+        return dt.replace(tzinfo=pytz.FixedOffset(dt_tz))
+
+
+def parse_int(x):
+    if x.isnumeric():
+        return int(x)
+
+    return x
+def logfile2df2(logfile):
+    data = pd.read_csv(
+        logfile,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None,
+        usecols=[0, 3, 4, 5, 6, 7, 8],
+        names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+        converters={
+            'time': parse_datetime,
+            'request': parse_str,
+            'status': parse_int,
+            'size': parse_int,
+            'referer': parse_str,
+            'user_agent': parse_str
+        }
+    )
+    print(data.shape, data.columns)
+    return data
+
 # Geoinfo hankimine ip-aadressi järgi
 def ipggeoinfo(ip_addr=''):
     ip_locate_url = 'https://geolocation-db.com/jsonp/' + ip_addr
@@ -81,7 +142,7 @@ def whoisinfo_asn_description(rows):
 def is_bot(rows):
     bots = ['bot', 'index']
     pat = rf'(?:{"|".join(bots)})'
-    return re.search(pat, str(rows.agent), re.IGNORECASE)!=None
+    return re.search(pat, str(rows.user_agent), re.IGNORECASE)!=None
 
 # Tagastab stringist sõna, millel on boti laadne nimi *bot, *index vms
 def find_bot_name(rows):
@@ -108,15 +169,17 @@ if __name__ == '__main__':
         path = os.path.dirname(sys.argv[0])
         logfile = os.path.join(path, 'valgalinn.access.log')
     print('Analüüsime logifaili:', logfile)
-    log_df = logfile2df(logfile)
+    log_df = logfile2df2(logfile)
+
+    # log_df = logfile2df(logfile)
     utc = pytz.utc
     now = utc.localize(datetime.now())
-    time24hoursago = now - timedelta(days=1)
+    time24hoursago = now - timedelta(days=3)
     log_df_filtered = log_df[log_df.time > time24hoursago]
 
     # Agendid aadressid allalaadimise mahu järgi
     print('Downloader Agents:')
-    result = log_df_filtered.groupby('agent')['resp_size']\
+    result = log_df_filtered.groupby('user_agent')['size']\
         .agg(['sum','count'])\
         .sort_values(by = ['sum'], ascending=[False])\
         .head(10)
@@ -126,10 +189,11 @@ if __name__ == '__main__':
 
     # IP aadressid allalaadimise mahu järgi
     print('Downloader IPaddresses traffic:')
-    result = log_df_filtered.groupby('IP_address')['resp_size']\
+    result = log_df_filtered.groupby('ip')['size']\
         .agg(['sum','count'])\
         .sort_values(by = ['sum'], ascending=[False])\
         .head(10)
+    print(result)
     result['asn_description'] = result.apply(whoisinfo_asn_description, axis=1)
     result['sum'] = result['sum'].map('{:,}'.format).str.replace(",", " ", regex=False).str.replace(".", ",", regex=False)
     print(result)
@@ -137,7 +201,7 @@ if __name__ == '__main__':
 
     # IP aadressid allalaadimise kordade järgi
     print('Downloader IPaddresses hits:')
-    result = log_df_filtered.groupby('IP_address')['resp_size'] \
+    result = log_df_filtered.groupby('ip')['size'] \
         .agg(['sum', 'count']) \
         .sort_values(by=['count'], ascending=[False]) \
         .head(10)
@@ -148,7 +212,7 @@ if __name__ == '__main__':
 
     # IP aadressid, kes said 403
     print('403 status:')
-    result = log_df_filtered[log_df_filtered['status_code'] == 403].groupby('IP_address')['resp_size'] \
+    result = log_df_filtered[log_df_filtered['status'] == 403].groupby('ip')['size'] \
         .agg(['count']) \
         .sort_values(by=['count'], ascending=[False]) \
         .head(30)
@@ -157,7 +221,7 @@ if __name__ == '__main__':
     print()
 
     print('404 status:')
-    result = log_df_filtered[log_df_filtered['status_code'] == 404].groupby('request')['resp_size'] \
+    result = log_df_filtered[log_df_filtered['status'] == 404].groupby('request')['size'] \
         .agg(['count']) \
         .sort_values(by=['count'], ascending=[False]) \
         .head(10)
@@ -165,7 +229,7 @@ if __name__ == '__main__':
     print()
 
     print('bots')
-    result = log_df_filtered[log_df_filtered.apply(is_bot, axis=1)].groupby('agent')['resp_size'] \
+    result = log_df_filtered[log_df_filtered.apply(is_bot, axis=1)].groupby('user_agent')['size'] \
         .agg(['sum','count'])\
         .sort_values(by = ['sum'], ascending=[False])\
         .head(10)
@@ -175,12 +239,12 @@ if __name__ == '__main__':
     print()
 
     print('tiles')
-    result = log_df_filtered[log_df_filtered.apply(is_tiles, axis=1)]['resp_size'] \
+    result = log_df_filtered[log_df_filtered.apply(is_tiles, axis=1)]['size'] \
         .agg(['sum', 'count'])
     print(result)
     print()
 
     # Viimase 24h kogumaht
-    log_df_filtered_resp_size_sum = log_df_filtered.resp_size.sum()
-    print(f'Päringuid {log_df_filtered.IP_address.count()}, kogumahuga {round(log_df_filtered_resp_size_sum/1024/1024)} Mb')
+    log_df_filtered_size_sum = log_df_filtered.size.sum()
+    print(f'Päringuid {log_df_filtered.ip.count()}, kogumahuga {round(log_df_filtered_size_sum/1024/1024)} Mb')
     # print()
