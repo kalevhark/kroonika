bd5a19f661cceed8d050a45617542975e78b2ac0
kalevhark <Kalev.Hark@mail.ee>
2024-02-27 14:08:22 +0200

    analyze fix

--- logs/analyze.py
+++ logs/analyze.py
@@ -1,8 +1,10 @@
+import asyncio
 from datetime import datetime, timedelta
 import json
 import os
 import re
 import sys
+import time
 import urllib.request
 
 from ipwhois import IPWhois, HTTPLookupError
@@ -106,7 +108,7 @@ def logfile2df2(logfile):
         converters={
             'time': parse_datetime,
             'request': parse_str,
-            'status': parse_str,
+            'status': parse_int,
             'size': parse_int,
             'referer': parse_str,
             'user_agent': parse_str
@@ -162,23 +164,40 @@ def is_tiles(rows):
     except:
         return False
 
-if __name__ == '__main__':
-    # path = os.path.dirname(sys.argv[0])
-    path = '/usr/local/apache2/logs/'
-    logfile = os.path.join(path, 'access_log')
-    if not os.path.isfile(logfile):
-        path = os.path.dirname(sys.argv[0])
-        # logfile = os.path.join(path, 'valgalinn.access.log')
-        logfile = os.path.join(path, 'access_log')
-    print('Analüüsime logifaili:', logfile)
-    log_df = logfile2df2(logfile)
+async def calc_results_downloader_agents(log_df_filtered):
+    # Agendid aadressid allalaadimise mahu järgi
+    result = log_df_filtered.groupby('user_agent')['size'] \
+        .agg(['sum', 'count']) \
+        .sort_values(by=['sum'], ascending=[False]) \
+        .head(10)
+    result['sum'] = result['sum'].map('{:,}'.format).str \
+        .replace(",", " ", regex=False).str.replace(".", ",", regex=False)
+    return ['Downloader Agents:', result]
 
-    # log_df = logfile2df(logfile)
-    utc = pytz.utc
-    now = utc.localize(datetime.now())
-    time24hoursago = now - timedelta(days=1)
-    log_df_filtered = log_df[log_df.time > time24hoursago]
+async def calc_results_ipaddresses_traffic(log_df_filtered):
+    # IP aadressid allalaadimise mahu järgi
+    result = log_df_filtered.groupby('ip')['size'] \
+        .agg(['sum', 'count']) \
+        .sort_values(by=['sum'], ascending=[False]) \
+        .head(10)
+    result['asn_description'] = result.apply(whoisinfo_asn_description, axis=1)
+    result['sum'] = result['sum'].map('{:,}'.format).str \
+        .replace(",", " ", regex=False).str.replace(".", ",", regex=False)
+    return ['Downloader IPaddresses traffic:', result]
 
+async def calc_results_ipaddresses_hits(log_df_filtered):
+    # IP aadressid allalaadimise kordade järgi
+    print('Downloader IPaddresses hits:')
+    result = log_df_filtered.groupby('ip')['size'] \
+        .agg(['sum', 'count']) \
+        .sort_values(by=['count'], ascending=[False]) \
+        .head(10)
+    result['asn_description'] = result.apply(whoisinfo_asn_description, axis=1)
+    result['sum'] = result['sum'].map('{:,}'.format).str \
+        .replace(",", " ", regex=False).str.replace(".", ",", regex=False)
+    return ['Downloader IPaddresses hits:', result]
+
+def calc_results(log_df_filtered):
     # Agendid aadressid allalaadimise mahu järgi
     print('Downloader Agents:')
     result = log_df_filtered.groupby('user_agent')['size']\
@@ -196,6 +215,7 @@ if __name__ == '__main__':
         .sort_values(by = ['sum'], ascending=[False])\
         .head(10)
     print(result)
+
     result['asn_description'] = result.apply(whoisinfo_asn_description, axis=1)
     result['sum'] = result['sum'].map('{:,}'.format).str.replace(",", " ", regex=False).str.replace(".", ",", regex=False)
     print(result)
@@ -212,9 +232,11 @@ if __name__ == '__main__':
     print(result)
     print()
 
+    print(log_df_filtered['status'].unique())
+
     # IP aadressid, kes said 403
     print('403 status:')
-    result = log_df_filtered[log_df_filtered['status'] == "403"].groupby('ip')['size'] \
+    result = log_df_filtered[log_df_filtered['status'] == 403].groupby('ip')['size'] \
         .agg(['count']) \
         .sort_values(by=['count'], ascending=[False]) \
         .head(30)
@@ -223,7 +245,7 @@ if __name__ == '__main__':
     print()
 
     print('404 status:')
-    result = log_df_filtered[log_df_filtered['status'] == "404"].groupby('request')['size'] \
+    result = log_df_filtered[log_df_filtered['status'] == 404].groupby('request')['size'] \
         .agg(['count']) \
         .sort_values(by=['count'], ascending=[False]) \
         .head(10)
@@ -255,3 +277,36 @@ if __name__ == '__main__':
     print(log_df_filtered[['time', 'ip', 'size']].resample("h", on='time').agg({'size': 'sum', 'ip': 'count'}))
 
     # print(log_df_filtered[['time', 'ip', 'size']].resample("5min", on='time').agg({'size': 'sum', 'ip': 'count'}).to_json(orient="records"))
+
+async def main():
+    path = '/usr/local/apache2/logs/'
+    logfile = os.path.join(path, 'access_log')
+    if not os.path.isfile(logfile):
+        path = os.path.dirname(sys.argv[0])
+        logfile = os.path.join(path, 'valgalinn.access.log')
+        # logfile = os.path.join(path, 'access_log')
+    print('Analüüsime logifaili:', logfile)
+    log_df = logfile2df2(logfile)
+
+    # log_df = logfile2df(logfile)
+    utc = pytz.utc
+    now = utc.localize(datetime.now())
+    time24hoursago = now - timedelta(days=1)
+    log_df_filtered = log_df[log_df.time > time24hoursago]
+
+    calc_results(log_df_filtered)
+
+    # res = await asyncio.gather(
+    #     calc_results_downloader_agents(log_df_filtered),
+    #     calc_results_ipaddresses_traffic(log_df_filtered),
+    #     calc_results_ipaddresses_hits(log_df_filtered),
+    # )
+    # for result in res:
+    #     print(result[0])
+    #     print(result[1])
+
+if __name__ == '__main__':
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
