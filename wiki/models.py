218359a772888414f79ccc89b7912bd2c8f719ab
kalevhark <Kalev.Hark@mail.ee>
2024-05-25 21:41:04 +0300

    pattern objects

--- wiki/models.py
+++ wiki/models.py
@@ -64,7 +64,7 @@ KUUD = (
 
 VIGA_TEKSTIS = '[?]'
 
-PATTERN_OBJECTS = r'\[([\wÀ-ÿ\s\"\-\,\.]+)\]\(\[(artikkel|isik|organisatsioon|objekt)_([0-9]*)\]\)'
+PATTERN_OBJECTS = r'\[([\wÀ-ÿ\s\"\-\,\.\(\)]+)\]\(\[(artikkel|isik|organisatsioon|objekt)_([0-9]*)\]\)'
 
 PREDECESSOR_DESCENDANT_NAMES = {
     'Isik': {
