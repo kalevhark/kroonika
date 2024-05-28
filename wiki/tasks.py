c74a85cdc9fafdd63fb24819087b64fc433cca1d
kalevhark <Kalev.Hark@mail.ee>
2024-05-28 19:34:07 +0300

    status code check

--- wiki/tasks.py
+++ wiki/tasks.py
@@ -1,7 +1,9 @@
 #!/home/ec2-user/django/kroonika_env/bin/python3
 
-#
-# Salvestatud kaardivaadete regulaarseks uuendamiseks
+# Regulaarselt iga minuti tagant käivitatav skript
+# 1. Kontroll kas veebiserver status 200
+# 2. Anonymoususeri jaoks avakuva cache
+# 3. Salvestatud kaardivaadete regulaarseks uuendamiseks (iga 10 minuti järel)
 # Käivitamiseks:
 # /python-env-path-to/python3 /path-to-wiki-app/tasks.py
 
@@ -10,6 +12,8 @@ from pathlib import Path
 
 import django
 from django.conf import settings
+import requests
+
 if __name__ == "__main__":
     import os
     # from django.test.utils import setup_test_environment
@@ -22,15 +26,8 @@ else:
 
 from django.contrib.auth.models import AnonymousUser, User
 from django.contrib.sessions.middleware import SessionMiddleware
+from django.core.mail import send_mail
 from django.test import Client, RequestFactory
-
-factory = RequestFactory()
-# Create an instance of a GET request.
-request = factory.get('/')
-middleware = SessionMiddleware(lambda x: x)
-middleware.process_request(request)
-request.session.save()
-request.user = AnonymousUser()
 
 try:
     from wiki.utils import shp_util
@@ -39,8 +36,35 @@ except: # kui käivitatakse lokaalselt
     from utils import shp_util
     from views import algus
 
+# kontrollime kas veebiserver annab korrektse vawtuse
+def test_status_200():
+    response = requests.head('https://valgalinn.ee')
+    if response.status_code != 200:
+        subject = f'Status = {response.status_code} from valgalinn.ee {datetime.now().strftime("%d.%m.%Y %H:%M:%S")}'
+        message = f'Status = {response.status_code}'
+        from_email, to = f'valgalinn.ee <{settings.DEFAULT_FROM_EMAIL}>', 'kalevhark@gmail.com'
+        send_mail(
+            subject,
+            message,
+            from_email,
+            [to],
+            fail_silently=False,
+        )
+
+# initsieerime avakuva salvestamise cache
+def get_algus():
+    factory = RequestFactory()
+    # Create an instance of a GET request.
+    request = factory.get('/')
+    middleware = SessionMiddleware(lambda x: x)
+    middleware.process_request(request)
+    request.session.save()
+    request.user = AnonymousUser()
+    algus(request)
+
 if __name__ == '__main__':
     now = datetime.now()
-    algus(request)
+    test_status_200()
+    get_algus()
     if now.minute % 10 == 0: # uuendame iga 10 minuti j2rel
         shp_util.kaardiobjektid2geojson()
