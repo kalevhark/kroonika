c9c867462693c950885ef7627f398672384c9f73
kalevhark <Kalev.Hark@mail.ee>
2024-06-25 00:34:11 +0300

    custom 500

--- kroonika/urls.py
+++ kroonika/urls.py
@@ -4,7 +4,7 @@ from ajax_select import urls as ajax_select_urls
 
 # from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls import handler500
+# from django.conf.urls import handler500
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
